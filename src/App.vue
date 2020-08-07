badb7a6671ffbbe88d6cae1a420a814ee07dca2a
Neel Raja <neelrr1@gmail.com>
2020-08-06 17:45:32 -0700

    Add dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" :class="dark ? 'dark' : 'light'">
     <br />
+    <!-- <b-button class="float" @click="toggleDarkMode()">Toggle dark mode!</b-button> -->
+    <div class="float p-1">  
+      <b-form-checkbox v-model="dark" switch>Toggle Dark Mode!</b-form-checkbox>
+    </div>
     <Home msg="Welcome to my site!" sub="(Definitely a WIP.)"/>
   </div>
 </template>
@@ -12,6 +16,11 @@ export default {
   name: 'App',
   components: {
     Home
+  },
+  data: function () {
+    return {
+      dark: true
+    }
   }
 }
 </script>
@@ -21,9 +30,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center;  
+  height: 100vh;
+  transition: background 1s, color 1s;
+  transition-timing-function: ease-out;
+}
+
+.dark {
   color: white;
   background: rgba(0, 0, 0, 0.86);
-  height: 100vh;
+}
+
+.float{
+	position:fixed;
+	bottom:40px;
+	right:40px;
+	background-color:#0C9;
+	border-radius:50px;
+	text-align:center;
 }
 </style>
